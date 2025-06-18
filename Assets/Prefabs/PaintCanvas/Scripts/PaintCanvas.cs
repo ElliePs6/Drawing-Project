3ccc257c0c42c1465a883da5bdeee34dff72f9e8
Eleftheria <eleuthpayne@gmail.com>
2025-06-18 10:20:32 +0300

    add grass

--- Assets/Prefabs/PaintCanvas/Scripts/PaintCanvas.cs
+++ Assets/Prefabs/PaintCanvas/Scripts/PaintCanvas.cs
@@ -3,14 +3,22 @@ using UnityEngine;
 public class PaintCanvas : MonoBehaviour
 {
     public Texture2D texture;
-    public Vector2 textureSize = new Vector2(x:2048, y: 2048);
+    public Vector2 textureSize = new Vector2(x: 2048, y: 2048);
 
 
-    void Start() { 
+    void Start()
+    {
 
         var r = GetComponent<Renderer>();
         texture = new Texture2D(width: (int)textureSize.x, height: (int)textureSize.y);
         r.material.mainTexture = texture;
 
     }
+
+    void OnCollisionEnter(Collision collision)
+{
+    Debug.Log("Collided with: " + collision.gameObject.name);
 }
+
+}
+
